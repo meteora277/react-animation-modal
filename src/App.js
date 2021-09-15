ac0790bee30f38f2f9bfe710e2ca876464d69f4c
Justin Diaz <justin.s.diaz@gmail.com>
2021-09-15 17:20:35 -0400

    created a really bad modal

--- src/App.js
+++ src/App.js
@@ -1,33 +1,108 @@
-import './App.css';
-import {createGlobalStyle} from 'styled-components';
-import React,{useState} from 'react';
-import {motion} from 'framer-motion'
+import "./App.css";
+import styled, { createGlobalStyle, css } from "styled-components";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
+import Button from "./Button";
 
-import Button from './Button'
+const button = {
+  rest: { scale: 1 },
+  hover: { scale: 1.3 },
+  pressed: { scale: 1.2 }
+};
 
 const GlobalStyle = createGlobalStyle`
-  body{
-    background:black; 
+  *,*::before,*::after{
+
+    box-sizing:border-box;
   }
-`
+
+  body{
+    height:100vh;
+    background:grey; 
+    display:grid;
+    place-items:center;
+  }
+`;
+const Overlay = styled.div`
+  position: absolute;
+  display: grid;
+  place-items: center;
+  background: black;
+  opacity: 0.6;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+`;
+const Modal = styled.div`.
+
+  width: 60%;
+  height:40%;
+  background: lightblue;
+  text-align: center;
+  font-size:2rem;
+  padding: 3rem;
+`;
 
 function App() {
-
-  const [ModalOpen, setModalOpen] = useState(false)
+  const [ModalOpen, setModalOpen] = useState(false);
 
   const handleClick = () => {
-    setModalOpen(!ModalOpen)
-    console.log(ModalOpen)
-  }
+    setModalOpen(!ModalOpen);
+    console.log(ModalOpen);
+  };
 
-  // const close = () => setModalOpen(false)
+  const close = () => setModalOpen(false);
   // const open = () => setModalOpen(true)
 
   return (
     <div>
-      <GlobalStyle/>
-      <motion.div><Button handleClick={handleClick}/></motion.div>
+      <GlobalStyle />
+
+      {ModalOpen ? null : (
+        <motion.div
+          variants={button}
+          initial="rest"
+          whileHover="hover"
+          whileTap="pressed"
+        >
+          <Button handleClick={handleClick} />
+        </motion.div>
+      )}
+
+
+      <AnimatePresence
+        initial={false}
+        exitBeforeEnter={true}
+        onExitComplete ={()=> null}
+      >
+        {ModalOpen ? (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Overlay>
+              <Modal>
+                <div>
+                  Hello there i am a modal and i would like to interest you in
+                  accepting our sites cookies, thanks{" "}
+                </div>
+                <motion.button
+                  variants={button}
+                  initial={button.rest}
+                  whileTap={button.pressed}
+                  whileHover={button.hover}
+                  onClick={() => close()}
+                >
+                  owo
+                </motion.button>
+              </Modal>
+            </Overlay>
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
     </div>
   );
 }
